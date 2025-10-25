f861b6ba2addebe6aab441fc27cd1fd8c4a5e210
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-10-25 11:31:39 +0530

    ready for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/heliolunar/', // 👈 your GitHub repo name
+  base: '/heliolunar/' // 👈 important
 })
